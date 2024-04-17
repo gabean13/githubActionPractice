6c620310bb985c9dddfbbcce754c759a9c4d218b
gabean13 <gazette9@naver.com>
2024-04-17 22:02:06 +0900

    create project

---
+++ src/main/java/com/githubtest/gabin/GabinApplication.java
@@ -0,0 +1,13 @@
+package com.githubtest.gabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubtest/gabin/GabinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubtest.gabin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
